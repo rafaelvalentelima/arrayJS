22100489a73c2fafd24d78c4b37e41e3cd2b0c24
rafael <rafaellimaat64@gmail.com>
2023-09-14 16:53:03 -0300

    Método Splice

---
+++ atualizando-lista.js
@@ -0,0 +1,6 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 2, "Rodrigo"); //Método para retirar nome. O terceiro parâmetro serve para incluír.
+nomes.push("Rafael"); // Método para incluir.
+
+console.log(nomes);
