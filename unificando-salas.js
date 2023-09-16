1e7f98183fd8181daf185b9e7b0a52fc79cbebce
rafael <rafaellimaat64@gmail.com>
2023-09-16 12:38:44 -0300

    Método Concat

--- unificando-salas.js
+++ unificando-salas.js
@@ -1,6 +1,6 @@
 const salaJS = ["Evaldo", "Camis", "Mari"];
 const salaPython = ["Ju", "Leo", "Raquel"];
 
-const salasUnificadas = salaJS.concat(salaPython);
+const salasJuntas = salaJS.concat(salaPython);
 
-console.log(salasUnificadas);
+console.log(salasJuntas);
