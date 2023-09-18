b796551b3fc1b3bdb9241262a926b42d39213c3c
rafael <rafaellimaat64@gmail.com>
2023-09-18 15:08:56 -0300

    Sintaxe de desistruturação de lista

--- lista-duas-dimenssoes.js
+++ lista-duas-dimenssoes.js
@@ -7,8 +7,8 @@ const listaDeAlunosEMedias = [alunos, medias];
 
 console.log(listaDeAlunosEMedias); // acessa toda a lista toda.
 console.log(
-  "A aluna da posição 1 d lista de alunos é " +
-    listaDeAlunosEMedias[0][1] +
-    " e a nota dessa aluna é " +
-    listaDeAlunosEMedias[1][1]
+  "A aluna da posição 1 d lista de alunos é ",
+  listaDeAlunosEMedias[0][1],
+  " e a nota dessa aluna é ",
+  listaDeAlunosEMedias[1][1]
 ); //acessa uma aluna específica.
--- procurando-aluno.js
+++ procurando-aluno.js
@@ -5,12 +5,17 @@ const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
   if (listaDeAlunosEMedias[0].includes(aluno)) {
-    // console.log(aluno + " está cadastrado");
+    // const alunos = listaDeAlunosEMedias[0]; // essa é uma forma mais poluída e que usa menos código.    ]
+    // const medias = listaDeAlunosEMedias[1];
 
-    const indice = listaDeAlunosEMedias[0].indexOf(aluno); //Método para pesquisar o indice.
+    const [alunos, medias] = listaDeAlunosEMedias; // Uma forma mais simplies de escrever o código acima: Criei uma const e atribuí os valores de listaDeAlunosEMedias.
+    //Sintaxe de desistruturação de Lista
 
-    const mediaDoAluno = listaDeAlunosEMedias[1][indice];
-    console.log(aluno + " tem a média: " + mediaDoAluno);
+    const indice = alunos.indexOf(aluno);
+
+    const mediaDoAluno = medias[indice];
+
+    console.log(aluno, " tem a média ", mediaDoAluno);
   } else {
     console.log("Aluno não encontrado");
   }
