48a48e4fc276957a29e04e726c9b99fe405f4e06
rafael <rafaellimaat64@gmail.com>
2023-09-18 17:23:14 -0300

    3 formas de fazer as Funções Callbacks

---
+++ callbacks.js
@@ -0,0 +1,17 @@
+const nomes = ["Evaldo", "Mari", "Camis"];
+
+nomes.forEach(function (nome) {
+  // Uma função Callback dentro de um forEach.
+  console.log(nome);
+});
+/////////////////////////////////////////////////////////////////////////////////
+nomes.forEach((nome) => {
+  //Segunda maneira da função Callback.
+  console.log(nome);
+});
+////////////////////////////////////////////////////////////////////////////////
+function imprimeNome(nome) {
+  console.log(nome);
+} // Terceira maneira de fazer uma função Callback
+
+nomes.forEach(imprimeNome); //Declarando a função, não pode ter os "()" de parâmetro, pois aqui só esta passando uma referência e não executar a função.
