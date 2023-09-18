134b31e44c2056e2d2568d54daa267b767d13dc1
rafael <rafaellimaat64@gmail.com>
2023-09-18 15:26:31 -0300

    Estrutura de repetição: For clássico.

---
+++ for.js
@@ -0,0 +1,8 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+// Primeira expressão: é executada apenas uma única vez.
+// Segunda expressão: condição de execução.
+// Terceira expressão: é executada sempre ao final do bloco.
+for (let indice = 0; indice < numeros.length; indice++) {
+  //numeros.length até o final do indice.
+  console.log(numeros[indice]);
+}
